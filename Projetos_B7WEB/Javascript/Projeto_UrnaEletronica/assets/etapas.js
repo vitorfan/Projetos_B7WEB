e01fc75298b2f3cf1624651b7454ecaede211c67
João Vitor Pereira de Negreiros <vitorfan2@hotmail.com>
2023-03-16 22:00:18 -0300

    Update 16/03/2023
    Urna eletrônica realizada com HTML/CSS e JavaScript.

---
+++ Projetos_B7WEB/Javascript/Projeto_UrnaEletronica/assets/etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '38111',
+                nome: 'Fulano de Tal',
+                partido: 'ABC',
+                fotos:[
+                    {url:'assets/images/38111.jpg', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '77222',
+                nome: 'Beltrano da Silva',
+                partido: 'DEFG',
+                fotos:[
+                    {url:'assets/images/77222.jpg', legenda: 'Vereador'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '99',
+                nome: 'Ciclano',
+                partido: 'ABC',
+                vice: 'Cic',
+                fotos:[
+                    {url:'assets/images/99.jpg', legenda: 'Prefeito'},
+                    {url:'assets/images/99_2.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: '84',
+                nome: 'Zulano',
+                partido: 'QWERTY',
+                vice: 'Zul',
+                fotos:[
+                    {url:'assets/images/84.jpg', legenda: 'Prefeito'},
+                    {url:'assets/images/84_2.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+        ]
+    }
+];
